d34c5d1bc38cf7aca9a5bc591274626f6d5ac2fe
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-02 08:07:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,7 +4,10 @@ WIN_COMBINATIONS = [
   [0, 4, 8], [2, 4, 6]
 ]
 def display_board(board)
-  puts " #{board[0]}"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  
 end
